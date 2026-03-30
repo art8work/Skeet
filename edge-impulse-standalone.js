e1b0ba73a2c6c80ffca813594a2c70be57d1f764
art8work <1riceeater@gmail.com>
2026-03-30 14:25:27 -0700

    v4

--- edge-impulse-standalone.js
+++ edge-impulse-standalone.js
@@ -1,6 +1,6 @@
 
 
-// The Module object: Our interface to the outside world. We import
+// The Module object: Our interface to the outside world. We import blah blah
 // and export values on it. There are various ways Module can be used:
 // 1. Not defined. We create it here
 // 2. A function parameter, function(Module) { ..generated code.. }
